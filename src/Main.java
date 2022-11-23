c090a9af4c316553c8162a7102cdcb359df0d488
Stern_Ritter <fadeef.k@yandex.ru>
2022-11-23 18:48:15 +0300

    feat/ add server start to Main class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
-public class Main {
+import exceptions.HttpClientException;
+import server.HttpTaskServer;
+import server.KVServer;
 
+import java.io.IOException;
+
+public class Main {
     public static void main(String[] args) {
+        try {
+            new KVServer().start();
+        } catch (IOException | HttpClientException ex) {
+            System.out.println("Не удалось запустить KVServer.");
+        }
+
+        try {
+            new HttpTaskServer().start();
+        } catch (IOException ex) {
+            System.out.println("Не удалось запустить HttpTaskServer.");
+        }
     }
 }
